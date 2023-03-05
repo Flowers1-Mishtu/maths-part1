4e1b4a589fd3d2e97eca870ce37274c39dc017f6
Flowers1-Mishtu <87982268+Flowers1-Mishtu@users.noreply.github.com>
2023-03-05 20:18:34 +0000

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -4,20 +4,20 @@ player2 = localStorage.getItem("player2");
 player1score = 0;
 player2score = 0;
 
-document.getElementById("player1name").innerHTML=player1+ ":";
-document.getElementById("player2name").innerHTML=player2+ ":";
+document.getElementById("player1name").innerHTML= player1 + " : " ;
+document.getElementById("player2name").innerHTML= player2 + " : " ;
 
 document.getElementById("player1score").innerHTML=player1score;
 document.getElementById("player2score").innerHTML=player2score;
 
-document.getElementById("playerquestion").innerHTML=" Question Turn -" + player1;
-document.getElementById("playeranswer").innerHTML=" Answer Turn -" + player2;
+document.getElementById("player_question").innerHTML=" Question Turn -" + player1;
+document.getElementById("player_answer").innerHTML=" Answer Turn -" + player2;
 
 function send(){
    number1 = document.getElementById("number1").value;
    number2 = document.getElementById("number2").value;
    actual_ansswer = parseInt(number1)*parseInt(number2);
-   question_number = "<h4>" + number1 + "X" + number2 + "</h4>";
+   question_number = "<h4>" + number1 + "x" + number2 + "</h4>";
    input_box = "<br>Answer : <input type= 'text' id= 'input_check_box'>";
    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button ";
    row= question_number + input_box + check_button;
@@ -26,43 +26,40 @@ function send(){
    document.getElementById("number2").value= "";
 
 }
-question_turn="player1";
-answer_turn="player2";
-function check(){
+question_turn="player1name";
+answer_turn="player2name";
+
+function check()
+{
     get_answer = document.getElementById("input_check_box").value;
-    answer = get_answer.toLowerCase();
-console.log(word,answer);
-
-    if (answer== word){
-        if(answer_turn == "player1")
+        
+    if(get_answer==actual_ansswer)
         {
-     player1score= player1score+1;
-     document.getElementById("player1score").innerHTML=player1score;
+            if(answer_turn == "player1name")
+                {
+                    update_player1_score = player1score +1;
+                    document.getElementById("player1score").innerHTML = update_player1_score;
+                }
+            else
+                {
+                    update_player2_score = player2score +1;
+                    document.getElementById("player2score").innerHTML = update_player2_score;
+                }
+
         }
+        if(question_turn == "player1name")
+            {
+                question_turn= "player2name";
+                answer_turn="player1name";
+                document.getElementById("player_question").innerHTML = "Question Turn - "+ player2;
+                document.getElementById("player_answer").innerHTML = "Answer Turn - "+ player1;
+            }
         else
-        {
-            player2score= player2score+1;
-            document.getElementById("player2score").innerHTML=player2score;
-               }
-    }
-    if (question_turn=="player1")
-    {
-        question_turn="player2";
-        document.getElementById("playerquestion").innerHTML=" Question Turn -" + player2;
-    }
-    else{
-        question_turn="player1";
-        document.getElementById("playerquestion").innerHTML=" Question Turn -" + player1;
-    }
+            {
+                question_turn = "player1name";
+                answer_turn="player2name";
+                document.getElementById("player_question").innerHTML = "Question Turn - "+ player1;
+                document.getElementById("player_answer").innerHTML = "Answer Turn - "+ player2;
+            }
 
-    if (answer_turn=="player1")
-    {
-        answer_turn="player2";
-        document.getElementById("playeranswer").innerHTML=" Answer Turn -" + player2;
-    }
-    else{
-        answer_turn="player1";
-        document.getElementById("playeranswer").innerHTML=" Answer Turn -" + player1;
-    }
-    document.getElementById("output").innerHTML=" ";
 }
