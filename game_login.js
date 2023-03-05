ba5304773cbd62124de7586abd50681e125aa564
Flowers1-Mishtu <87982268+Flowers1-Mishtu@users.noreply.github.com>
2023-03-05 16:37:40 +0000

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -14,28 +14,16 @@ document.getElementById("playerquestion").innerHTML=" Question Turn -" + player1
 document.getElementById("playeranswer").innerHTML=" Answer Turn -" + player2;
 
 function send(){
-    get_word = document.getElementById("word").value;
-    word = get_word.toLowerCase();
-
-    charAt1 = word.charAt(1);
-
-    length_divide_2 = Math.floor(word.length/2);
-    charAt2 = word.charAt(length_divide_2);
-
-    length_minus_1= word.length - 1;
-    charAt3 = word.charAt(length_minus_1);
-
-    remove_charAt1 = word.replace(charAt1, "_")
-    remove_charAt2 = remove_charAt1.replace(charAt2,"_");
-    remove_charAt3 = remove_charAt2.replace(charAt3, "_")
-
-    question_word = "<h4 id='word_diplay'> Q."+remove_charAt3+"</h4>";
-    input_box= "<br>Answer : <input type= 'text' id='input_check_box'>";
-    check_button = "<br><br><button class = 'btn btn-info' onclick='check()'>Check</button>";
-    row= question_word + input_box+ check_button;
-    document.getElementById("output").innerHTML= row;
-    document.getElementById("word").value = " ";
-
+   number1 = document.getElementById("number1").value;
+   number2 = document.getElementById("number2").value;
+   actual_ansswer = parseInt(number1)*parseInt(number2);
+   question_number = "<h4>" + number1 + "X" + number2 + "</h4>";
+   input_box = "<br>Answer : <input type= 'text' id= 'input_check_box'>";
+   check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button ";
+   row= question_number + input_box + check_button;
+   document.getElementById("output").innerHTML = row;
+   document.getElementById("number1").value= "";
+   document.getElementById("number2").value= "";
 
 }
 question_turn="player1";
